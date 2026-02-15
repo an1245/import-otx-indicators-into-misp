ccf2a17969303511dfafc748ba2cf50087172a23
andy <127995147+an1245@users.noreply.github.com>
2026-02-15 07:13:22 +0000

    Add configuration settings for PyMISP and OTX

---
+++ config.py
@@ -0,0 +1,12 @@
+# ---- PyMISP Configuration ----
+MISP_URL = "{insert MISP url}"
+MISP_API_KEY = "{insert MISP API key}"
+MISP_VERIFY_CERT = False
+EVENT_ID = {insert MISP Event ID}
+
+# ---- OTX Configuration ----
+OTX_API_KEY = "{insert OTX API key}" 
+
+# ---- Import Configuration ----
+DAYS=1              # number of days to import.  works best if you import 1 day and run every day
+DECAY_DAYS=120      # don't import events that are older than x days ( should align with lifetime days in your decay model )
